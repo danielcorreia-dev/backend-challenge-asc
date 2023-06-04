8c8ce6be51b4eb95bb8c42af72dbad17d0e404f5
Daniel Correia <100008358+danielcorreia-dev@users.noreply.github.com>
2023-06-04 19:40:05 -0300

    feat: added seeders and factories for users and a scaffold for properties

---
+++ database/factories/PropertyFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Property;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Property>
+ */
+class PropertyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Property::class;
+
+    public function definition(): array
+    {
+        return [];
+    }
+}
