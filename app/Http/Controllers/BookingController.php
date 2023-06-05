3a8575b2e62c5f146a8fd37040222ee76d7c31ae
Daniel Correia <100008358+danielcorreia-dev@users.noreply.github.com>
2023-06-05 07:00:39 -0300

    feat: resource controllers for booking, property and user

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,16 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\Booking\BookingServiceContract;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    //
-    protected $bookingService;
-
-    public function __construct(BookingServiceContract $bookingServiceContract)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $this->bookingService = $bookingServiceContract;
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,30 +2,70 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
-use App\Models\User;
-use Illuminate\Http\Request;
 use App\Services\User\UserServiceContract;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
     protected $userService;
 
     public function __construct(UserServiceContract $userServiceContract)
     {
         $this->userService = $userServiceContract;
     }
-
-    public function getAllUsers()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        try {
-            return response()->json(
-                $this->userService->getAllUsers(),
-                200
-            );
-        } catch (Exception $e) {
-            return response()->json($e->getMessage(), $e->getCode());
-        }
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
