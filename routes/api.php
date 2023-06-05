b103ac4ccee62f36940c043885b17930c7b674fd
Daniel Correia <100008358+danielcorreia-dev@users.noreply.github.com>
2023-06-05 07:01:37 -0300

    feat: added a an api resource route for each controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('/users')->group(function () {
-    Route::get('/', [UserController::class, 'getAllUsers']);
-});
+Route::apiResource('users', UserController::class);
+Route::apiResource('bookings', BookingController::class);
+Route::apiResource('properties', PropertyController::class);
